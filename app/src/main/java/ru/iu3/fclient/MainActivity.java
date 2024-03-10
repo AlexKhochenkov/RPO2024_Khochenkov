b76004e11d3b4c3206ce3718bf196bd7d45b043d
Alexey Khochenkov <alekshoch@yandex.ru>
2024-03-10 19:54:32 +0300

    1 lab

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
@@ -23,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] v = randomBytes(16);
+        byte[] key = randomBytes(16);
+        byte[] encryptedV = encrypt(key, v);
+        byte[] decryptV = decrypt(key, encryptedV);
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
@@ -33,4 +40,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] array);
+    public static native byte[] decrypt(byte[] key, byte[] array);
 }
